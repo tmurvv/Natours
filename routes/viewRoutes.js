a211d080c364b080dd75afa400b095d7f79e61ce
tmurvv <tmurv@shaw.ca>
2019-09-26 10:17:54 -0600

    bug fix remove bookingController middleware

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const viewsController = require('./../controllers/viewsController');
 const authController = require('../controllers/authController');
-const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
-router.get(
-    '/',
-    bookingController.createBookingCheckout,
-    authController.isLoggedIn,
-    viewsController.getOverview
-);
+router.get('/', authController.isLoggedIn, viewsController.getOverview);
 router.get(
     '/tour/:slug',
     authController.isLoggedIn,
@@ -18,12 +12,7 @@ router.get(
 );
 router.get('/login', authController.isLoggedIn, viewsController.getLogin);
 router.get('/me', authController.protect, viewsController.getAccount);
-router.get(
-    '/my-tours',
-    //bookingController.createBookingCheckout,
-    authController.protect,
-    viewsController.getMyTours
-);
+router.get('/my-tours', authController.protect, viewsController.getMyTours);
 
 router.post(
     '/submit-user-data',
