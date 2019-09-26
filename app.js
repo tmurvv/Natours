411531ee6738f6436314c6b6622fc8f04a995527
tmurvv <tmurv@shaw.ca>
2019-09-26 09:10:39 -0600

    implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -25,6 +26,17 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 //GLOBAL MIDDLEWARE
+// implement CORS - adds Access-Control-Allow-Origin everywhere
+app.use(cors());
+app.options('*', cors());
+// to allow access to one or a few origins:
+// app.use(
+//     cors({
+//         origin: 'https://www.natours.com'
+//     })
+// );
+// app.options('/api/v1/tours/:id', cors());
+
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
